1527452a5eee47078c9937752ac10bff6f860cdd
Acer <@Ramina_k>
2025-04-24 00:53:19 +0500

    Initial structure of MyHashTable with HashNode

---
+++ src/MyHashTable.java
@@ -0,0 +1,37 @@
+public class MyHashTable<K, V> {
+
+    private static class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    protected HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % M;
+    }
+}
