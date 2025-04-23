2fbbc8138ac9a8c742621d59363490bff35e793f
Acer <@Ramina_k>
2025-04-24 00:56:10 +0500

    Implemented put(), get(), remove(), contains(), getKey()

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -34,4 +34,76 @@ public class MyHashTable<K, V> {
     private int hash(K key) {
         return Math.abs(key.hashCode()) % M;
     }
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+        size++;
+        head = chainArray[index];
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = head;
+        chainArray[index] = newNode;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+        while (head != null) {
+            if (head.key.equals(key)) return head.value;
+            head = head.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+        HashNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key)) break;
+            prev = head;
+            head = head.next;
+        }
+
+        if (head == null) return null;
+        size--;
+
+        if (prev != null) {
+            prev.next = head.next;
+        } else {
+            chainArray[index] = head.next;
+        }
+
+        return head.value;
+    }
+
+    public boolean contains(V value) {
+        for (HashNode<K, V> node : chainArray) {
+            while (node != null) {
+                if (node.value.equals(value)) return true;
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (HashNode<K, V> node : chainArray) {
+            while (node != null) {
+                if (node.value.equals(value)) return node.key;
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
 }
